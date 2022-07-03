5ee1e63298f09f6fc1dd7f4f023937c90fb0227c
Ashutosh Lohani <54906517+ashutoshlohani@users.noreply.github.com>
2022-07-03 22:45:48 +0530

    added spinnner and some responsivenes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import * as React from 'react';
 import './styles/App.scss';
+import { FaSpinner } from 'react-icons/fa';
 
 function Form({ onSubmit, buttonText }) {
+   const [visible, setVisible] = React.useState(false);
+
    function handleSubmit(event) {
       event.preventDefault();
       const { username, password } = event.target.elements;
@@ -10,20 +13,27 @@ function Form({ onSubmit, buttonText }) {
          username: username.value,
          password: password.value,
       });
+
+      setVisible(true);
    }
 
    return (
       <form onSubmit={handleSubmit}>
-         <div>
+         <div className='input-container'>
             <label htmlFor='username'>Username</label>
+            <br />
             <input id='username' />
          </div>
-         <div>
+         <div className='input-container'>
             <label htmlFor='password'>Password</label>
+            <br />
             <input id='password' type='password' />
          </div>
-         <div>
-            <button type='submit'>{buttonText}</button>
+         <div className='button-container'>
+            <button type='submit'>
+               {buttonText}
+               {visible && <FaSpinner className='spinner' />}
+            </button>
          </div>
       </form>
    );
@@ -36,15 +46,32 @@ function LoginForm({ changeState }) {
 
    return (
       <div className='form-container'>
-         <h1>Sign in</h1>
-         <h3>
-            New user?
-            <span onClick={changeState}>Register here 👈</span>
-         </h3>
-         <Form onSubmit={login} buttonText='Submit' />
-         <span>---------- or ---------</span>
-         <div>
-            <button>Continue with Google</button>
+         <div className='top'>
+            <h1>Sign in</h1>
+            <h3>
+               New user?{'  '}
+               <span onClick={changeState}>Register here 👈</span>
+            </h3>
+         </div>
+         <div className='middle'>
+            <Form onSubmit={login} buttonText='Submit' />
+         </div>
+         <div className='bottom'>
+            <div className='or'>
+               <hr />
+               <span>Or</span>
+               <hr />
+            </div>
+
+            <div>
+               <button>
+                  <img
+                     alt='google'
+                     src='https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png'
+                  />
+                  Continue with Google
+               </button>
+            </div>
          </div>
       </div>
    );
@@ -57,15 +84,31 @@ function RegisterForm({ changeState }) {
 
    return (
       <div className='form-container'>
-         <h1>Create an account</h1>
-         <h3>
-            Already a user?
-            <span onClick={changeState}>Sign in 👈</span>
-         </h3>
-         <Form onSubmit={register} buttonText='Register' />
-         <span>---------- or ---------</span>
-         <div>
-            <button>Continue with Google</button>
+         <div className='top'>
+            <h1>Create an account</h1>
+            <h3>
+               Already a user?{'  '}
+               <span onClick={changeState}>Sign in 👈</span>
+            </h3>
+         </div>
+         <div className='middle'>
+            <Form onSubmit={register} buttonText='Register' />
+         </div>
+         <div className='bottom'>
+            <div className='or'>
+               <hr />
+               <span>Or</span>
+               <hr />
+            </div>
+            <div>
+               <button>
+                  <img
+                     alt='google'
+                     src='https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png'
+                  />
+                  Continue with Google
+               </button>
+            </div>
          </div>
       </div>
    );
@@ -77,8 +120,10 @@ function App() {
    return (
       <div className='landing-page'>
          <div className='left-side'>
-            <h1>Book shelf Logo</h1>
-            <p>Sign in or register</p>
+            <div className='logo-continer'>
+               <h1>BookShelf</h1>
+               <p>Find your next book from our shelf</p>
+            </div>
          </div>
 
          <div className='right-side'>
